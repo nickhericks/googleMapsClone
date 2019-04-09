3c62b94d90e8322359a333ac90f494d78457cc75
Nick-Hericks <nickhericks@gmail.com>
2019-04-09 13:36:09 +0200

    Create fetchWithJSONP function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 /* globals google */
 /* eslint-disable no-new  */
 
-// # Start editing your JavaScript here
-// ===============
+
+
+
+const fetchWithJSONP = (uri) => {
+	const script = document.createElement('script')
+	script.src = uri;
+	document.body.appendChild(script)
+	document.body.removeChild(script)
+}
+
+fetchWithJSONP('https://maps.googleapis.com/maps/api/js?key=AIzaSyDvmbsNbSZIMfMw8lj2GEo0fJAlO8wfk0o&callback=initMap');
+
+
+
 
 function initMap () {
   const mapDiv = document.querySelector('#map')
   new google.maps.Map(mapDiv, {
-    center: { lat: 1.3521, lng: 103.8198 },
-    zoom: 13
+		center: { lat: -25.9483026, lng: 32.6020333 },
+    zoom: 15
   })
 }
